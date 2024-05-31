72c2b4ce19e3054eb709b7ec935d5d2a6934725c
philosan <philopaterwaheed9@gmail.com>
2024-05-31 12:57:15 -0400

    fisinsed till checking output

--- program.hpp
+++ program.hpp
@@ -1,10 +1,10 @@
-
-#pragma once
-#include <string>
+#include <string> 
+#pragma once                                                               
 class program {
-public:
-  std::string file_name;
-  std::string input_file;
-  std::string output_file;
-  std::string url;
+   public :                                                                    
+   std::string file_name;
+   std::string input_file;
+   std::string output_file;
+   std::string url;
+  
 };
